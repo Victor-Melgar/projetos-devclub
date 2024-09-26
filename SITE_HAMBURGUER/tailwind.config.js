b697f762a68435767f69e495c9c25f006cf70588
Victor-Melgar <gabrielmelgar.digital@gmail.com>
2024-09-26 14:06:11 -0400

    projetos-devclub

---
+++ SITE_HAMBURGUER/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/SITE_HAMBURGUER/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
